015d5ec238d38bba918427c93135395705a4026e
Fabien <fabien@Fabiens-Mac-mini.local>
2024-04-16 18:46:01 +0200

    update

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,18 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fzutter <fzutter@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fabien <fabien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:38:08 by fzutter           #+#    #+#             */
-/*   Updated: 2024/04/09 15:23:27 by fzutter          ###   ########.fr       */
+/*   Updated: 2024/04/16 16:25:24 by fabien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#   define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stddef.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
