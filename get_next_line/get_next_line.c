656664c79fd44a5a4e27be2ca9fb184330c65be7
Fabien <fabien@Fabiens-Mac-mini.local>
2024-04-24 21:33:27 +0200

    asda

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fzutter <fzutter@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fabien <fabien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/09 10:37:54 by fzutter           #+#    #+#             */
-/*   Updated: 2024/04/23 16:29:29 by fzutter          ###   ########.fr       */
+/*   Created: 2024/04/01 10:37:54 by fzutter           #+#    #+#             */
+/*   Updated: 2024/04/24 21:30:09 by fabien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*temp_line(int fd, char *buf, char *backup)
+static char	*temp_line(int fd, char *backup, char *buf)
 {
 	ssize_t		read_line;
 	char		*char_temp;
@@ -21,25 +21,20 @@ static char	*temp_line(int fd, char *buf, char *backup)
 	while (read_line > 0)
 	{
 		read_line = read(fd, buf, BUFFER_SIZE);
-//		printf("Read line : %d\n", read_line);
 		if (read_line == -1)
 			return (NULL);
 		else if (read_line == 0)
 			break ;
-//		printf("Buf1 : %s\n", buf);
 		buf[read_line] = '\0';
-//		printf("Buf2 : %s\n", buf);
 		if (backup == NULL)
 			backup = ft_strdup("");
 		char_temp = backup;
-//		printf("Backup1 : %s\n", backup);
 		backup = ft_strjoin(char_temp, buf);
 		free (char_temp);
 		char_temp = NULL;
 		if (ft_strchr (buf, '\n'))
 			break ;
 	}
-//	printf("Backup2 : %s\n", backup);
 	return (backup);
 }
 
@@ -49,7 +44,6 @@ static char	*extract(char *line)
 	char	*temp;
 
 	count = 0;
-//	printf("Line : %s\n", line);
 	while (line[count] != '\n' && line[count] != '\0')
 		count++;
 	if (line[count] == 0 || line[1] == 0)
@@ -61,7 +55,6 @@ static char	*extract(char *line)
 		temp = NULL;
 	}
 	line[count + 1] = 0;
-//	printf("Temp : %s\n", temp);
 	return (temp);
 }
 
@@ -71,21 +64,22 @@ char	*get_next_line(int fd)
 	char		*buf;
 	static char	*backup;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(backup);
+		backup = NULL;
+		buf = NULL;
 		return (NULL);
+	}
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buf == NULL)
 		return (NULL);
-	line = temp_line(fd, buf, backup);
-//	printf("Buf : %s\n", buf);
-//	printf("Line : %s\n", line);
+	line = temp_line(fd, backup, buf);
 	free(buf);
 	buf = NULL;
 	if (line == NULL)
 		return (NULL);
 	backup = extract(line);
-//	printf("Line : %s\n", line);
-//	printf("Backup : %s\n", backup);
 	return (line);
 }
 /*
@@ -94,7 +88,7 @@ int	main(void)
 	int		fd;
 	char	*line;
 
-	fd = open("../workspace/text.txt", O_RDONLY);
+	fd = open("text", O_RDONLY);
 	while (1)
 	{
 		line = get_next_line(fd);
